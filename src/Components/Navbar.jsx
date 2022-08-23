e72de9ce4d2c64d93497361fb2fbdb93831e7982
Srk78699 <ksirtaj29@gmail.com>
2022-08-23 19:01:17 +0530

    projectinitialization

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,120 @@
+import {
+  Box,
+  Img,
+  SimpleGrid,
+  Text,
+  Flex,
+  Center,
+  
+} from "@chakra-ui/react";
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./Navbar.module.css";
+import {
+  faLocationArrow,
+  faStar,
+  faDownload,
+  faPenSquare,
+  faHospitalSymbol,
+} from "@fortawesome/fontawesome-free-solid";
+import { NavLink } from "react-router-dom";
+// import { faLocationArrow, faStar,faDownload, faSquare, faPenSquare, faHospitalSymbol , faMap } from "@fortawesome/fontawesome-free-regular";
+
+const links = [
+  { path: "/sale", title: "SALE" },
+
+  { path: "/makeup", title: "MAKEUP" },
+  { path: "/skincare", title: "SKINCARE" },
+  { path: "/fragrance", title: "FRAGRANCE" },
+  { path: "/haircare", title: "HAIRCARE" },
+  { path: "/tool&brushes", title: "TOOLS & BRUSHES" },
+  { path: "/brands", title: "BRANDS" },
+];
+const Navbar = () => {
+  return (
+    <>
+      <SimpleGrid>
+        <Box>
+          {/* <Box  display="flex" alignItems="center" bg="#F3F3F3" h='50px' w='100%'> */}
+          <Flex bg="#F3F3F3" h="50px" w="100%">
+            <Center>
+              <Box marginLeft={"60px"}>
+                <Img
+                  w="80%"
+                  h="66px"
+                  src="https://cdn02.nnnow.com/web-images/master/navtree_metaData/59b2425ae4b0d70964ee66e0/1505806763887/12NNNOWLOGODESKTOP.png"
+                  alt="img"
+                />
+              </Box>
+            </Center>
+            <Center marginRight={"180px"}>
+              <Text>
+                <FontAwesomeIcon icon={faLocationArrow} />
+                STORE LOCATOR
+              </Text>
+            </Center>
+            <Center>
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <Text w="63%" marginLeft={"10px"} marginRight="10px">
+                Get 10% OFF on your first purchase. Use Code: BEAUTY10
+              </Text>
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+              <FontAwesomeIcon icon={faStar} />
+            </Center>
+            {/* </Box> */}
+            <Center marginLeft={"40px"}>
+              <FontAwesomeIcon icon={faDownload} /> <Text>Get App</Text>
+            </Center>
+            <Center marginLeft={"40px"}>
+              <FontAwesomeIcon icon={faPenSquare} />
+              <Text marginLeft={"5px"}>Track Order</Text>
+            </Center>
+            <Center marginLeft={"30px"}>
+              <FontAwesomeIcon icon={faHospitalSymbol} />
+              <Text>Loyalty</Text>
+            </Center>
+          </Flex>
+          <Box bg={"white"} h="100px">
+            <Flex justifyContent="space-between">
+              <Center>
+                <Text>Search Bar</Text>
+              </Center>
+              <Center>
+                <Text fontSize={"60px"} h="10px">
+                  SEPHORA
+                </Text>
+              </Center>
+              <Flex>
+                <Text>Heart</Text>
+                <Text>Cart</Text>
+                <Text>Login</Text>
+              </Flex>
+            </Flex>
+          </Box>
+        </Box>
+        <Box fontSize={"20px"}>
+        <Flex justifyContent="space-evenly">
+        {links.map((link) => (
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? styles.active : styles.default
+          }
+          key={link.path}
+          to={link.path}
+        >
+          {link.title}
+        </NavLink>
+        ))}
+
+        </Flex>
+        </Box>
+      </SimpleGrid>
+    </>
+  );
+};
+
+export default Navbar;
